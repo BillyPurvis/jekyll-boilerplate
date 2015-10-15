2bcd75fee4de5a8b7efa6c71c5d2312c3395568b
Billy Purvis <purvisbilly@outlook.com>
2015-10-15 15:36:37 +0100

    Updates

---
+++ _site/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    path            = require('gulp-path'),
+    util            = require('gulp-util'),
+    spawn           = require('child_process').spawn,
+    rename          = require('gulp-rename'),
+    connect         = require('gulp-connect'),
+    minifycss       = require('gulp-minify-css'),
+    autoprefixer    = require('gulp-autoprefixer');
+    log             = util.log;
+  
+
+// Config
+var ports = {
+    dev: 4000,
+    dist: 4040
+};
+
+var paths = {
+    site: "_site",
+    css: "./css/",
+    jekyll: ['_layouts/*.html', '_posts/*', '_sites']
+};
+
+
+
+gulp.task('connect', function () {
+    'use strict';
+    log("Serve those files");
+    connect.server({
+        root: paths.site,
+        livereload: true,
+        port: ports.dev
+    });
+});
+ 
+gulp.task("sass", function () {
+    'use strict';
+    log("Generating CSS files " + (new Date()).toString());
+    gulp.src("./css/main.scss")
+		.pipe(sass({ style: 'expanded' }))
+					.pipe(autoprefixer("last 3 version", "safari 5", "ie 8", "ie 9"))
+		.pipe(gulp.dest("./css"))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest("./css"));
+});
+
+gulp.task("watch", function () {
+    'use strict';
+    log("Watching Sass Files");
+    gulp.watch("./css/main.scss", ["sass"]);
+});
+
+gulp.task('default', ['connect', 'sass']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,58 @@
+/*jslint node: true */
 var gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     path            = require('gulp-path'),
+    util            = require('gulp-util'),
+    spawn           = require('child_process').spawn,
     rename          = require('gulp-rename'),
     connect         = require('gulp-connect'),
-    minifyCss       = require('gulp-minify-css'),
-    autoprefixer       = require('gulp-autoprefixer');
+    minifycss       = require('gulp-minify-css'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    log             = util.log;
+  
 
 // Config
-
 var ports = {
-    dev: 9090,
-    dist: 9091
+    dev: 4000,
+    dist: 4040
 };
+
+var paths = {
+    jekyll: "./_site"
+};
+
+gulp.task('jekyll', function () {
+    'use strict';
+    spawn('jekyll', ['build'], {stdio: 'inherit'});
+});
+
+gulp.task("sass", function () {
+    'use strict';
+    log("Generating CSS files " + (new Date()).toString());
+    gulp.src("./css/main.scss")
+		.pipe(sass({ style: 'expanded' }))
+					.pipe(autoprefixer("last 3 version", "safari 5", "ie 8", "ie 9"))
+		.pipe(gulp.dest("./css"))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest("./css"));
+});
+
+gulp.task("watch", function () {
+    'use strict';
+    log("Watching Sass Files");
+    gulp.watch("./css/main.scss", ["sass"]);
+});
 
 gulp.task('connect', function () {
     'use strict';
-    connect.server();
+    log("Serve those files");
+    connect.server({
+        root: paths.jekyll,
+        livereload: true,
+        port: ports.dev
+    });
 });
- 
-gulp.task('default', ['connect']);
+
+
+gulp.task('default', ['connect', 'sass']);
